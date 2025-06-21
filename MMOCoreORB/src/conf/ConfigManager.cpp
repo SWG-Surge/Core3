bc3a986296f477f90ef9ea4d558a925aab5c6369
KimoSWG <crimson1125@gmail.com>
2025-06-21 08:26:03 -0500

    and again...qqq

--- MMOCoreORB/src/conf/ConfigManager.cpp
+++ MMOCoreORB/src/conf/ConfigManager.cpp
@@ -20,7 +20,139 @@ ConfigManager::~ConfigManager() {
 	clearConfigData();
 }
 
-#include "full/ConfigManagerMethods.cpp"
+// All ConfigManager methods restored below
+
+void ConfigManager::clearConfigData() {
+	Locker guard(&mutex);
+
+	for (int i = 0; i < configData.size(); ++i) {
+		auto entry = configData.getUnsafe(i).getValue();
+		delete entry;
+	}
+
+	configData.removeAll();
+	configData.setNoDuplicateInsertPlan();
+	incrementConfigVersion();
+}
+
+bool ConfigManager::getBool(const String& name, bool defaultValue, unsigned int accountID) {
+	ReadLocker guard(&mutex);
+	ConfigDataItem* itm = findItem(name, accountID);
+	if (itm == nullptr)
+		return defaultValue;
+	return itm->getBool();
+}
+
+int ConfigManager::getInt(const String& name, int defaultValue, unsigned int accountID) {
+	ReadLocker guard(&mutex);
+	ConfigDataItem* itm = findItem(name, accountID);
+	if (itm == nullptr)
+		return defaultValue;
+	return itm->getInt();
+}
+
+float ConfigManager::getFloat(const String& name, float defaultValue, unsigned int accountID) {
+	ReadLocker guard(&mutex);
+	ConfigDataItem* itm = findItem(name, accountID);
+	if (itm == nullptr)
+		return defaultValue;
+	return itm->getFloat();
+}
+
+const String& ConfigManager::getString(const String& name, const String& defaultValue, unsigned int accountID) {
+	Locker guard(&mutex);
+	ConfigDataItem* itm = findItem(name, accountID);
+	if (itm == nullptr) {
+		itm = new ConfigDataItem(defaultValue);
+		if (itm == nullptr || !updateItem(name, itm))
+			throw Exception("ConfigManager::getString(" + name + ") failed to set default value: [" + defaultValue + "]");
+	}
+	return itm->getString();
+}
+
+const Vector<String>& ConfigManager::getStringVector(const String& name, unsigned int accountID) {
+	ReadLocker guard(&mutex);
+	ConfigDataItem* itm = findItem(name, accountID);
+	if (itm == nullptr)
+		throw Exception("ConfigManager::getStringVector(" + name + ") not found");
+	return itm->getStringVector();
+}
+
+const SortedVector<String>& ConfigManager::getSortedStringVector(const String& name, unsigned int accountID) {
+	ReadLocker guard(&mutex);
+	ConfigDataItem* itm = findItem(name, accountID);
+	if (itm == nullptr)
+		throw Exception("ConfigManager::getSortedStringVector(" + name + ") not found");
+	return itm->getSortedStringVector();
+}
+
+int ConfigManager::getUsageCounter(const String& name) const {
+	ConfigDataItem* itm = findItem(name);
+	if (itm == nullptr)
+		return -1;
+	return itm->getUsageCounter();
+}
+
+bool ConfigManager::setBool(const String& name, bool newValue) {
+	return updateItem(name, new ConfigDataItem(newValue));
+}
+
+bool ConfigManager::setInt(const String& name, int newValue) {
+	return updateItem(name, new ConfigDataItem(newValue));
+}
+
+bool ConfigManager::setFloat(const String& name, float newValue) {
+	return updateItem(name, new ConfigDataItem(newValue));
+}
+
+bool ConfigManager::setString(const String& name, const String& newValue) {
+	return updateItem(name, new ConfigDataItem(newValue));
+}
+
+bool ConfigManager::setStringFromFile(const String& name, const String& fileName) {
+	StringBuffer newValue;
+	try {
+		File file(fileName);
+		FileReader reader(&file);
+		String line;
+		while (reader.readLine(line))
+			newValue << line;
+		reader.close();
+		return setString(name, newValue.toString());
+	} catch (const FileNotFoundException& e) {
+		error("setStringFromFile(" + name + ", " + fileName +") File Not Found.");
+	} catch (const Exception& e) {
+		error("setStringFromFile(" + name + ", " + fileName +") Unexpected exception reading file.");
+	}
+	return false;
+}
+
+bool ConfigManager::updateItem(const String& name, ConfigDataItem* newItem) {
+	Locker guard(&mutex);
+	if (newItem == nullptr || name.isEmpty())
+		return false;
+	int pos = configData.find(name);
+	if (pos != -1) {
+		ConfigDataItem* oldItem = configData.get(pos);
+		configData.drop(name);
+		delete oldItem;
+		oldItem = nullptr;
+	}
+	if (logChanges) {
+		if (isSensitiveKey(name)) {
+			info(true) << "Configuration updated: " << name;
+		} else {
+			info(true) << "Configuration update: " << name << " = [" << newItem->toString() << "]";
+		}
+	}
+#ifdef DEBUG_CONFIGMANAGER
+	info("updateItem: " + name + " = [" + newItem->toString() + "]", true);
+	newItem->setDebugTag(name);
+#endif // DEBUG_CONFIGMANAGER
+	configData.put(std::move(name), std::move(newItem));
+	incrementConfigVersion();
+	return true;
+}
 
 bool ConfigManager::loadConfigData() {
 	Locker guard(&mutex);
