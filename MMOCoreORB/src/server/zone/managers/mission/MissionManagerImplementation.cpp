61a4e85a0a7bdedf6400c0940b921ffdbd1bc66b
KimoSWG <crimson1125@gmail.com>
2025-06-21 20:00:49 -0500

    but i am le tired.  then have a nap.  then fix the mISSION

--- MMOCoreORB/src/server/zone/managers/mission/MissionManagerImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/mission/MissionManagerImplementation.cpp
@@ -946,6 +946,26 @@ void MissionManagerImplementation::randomizeGenericDestroyMission(CreatureObject
 	else
 		missionType = "_creature";
 
+	// Get the target creature template name from the lair template
+	String targetTemplate = "";
+	const Vector<String>* mobiles = lairTemplateObject->getWeightedMobiles();
+	if (mobiles != nullptr && mobiles->size() > 0) {
+		targetTemplate = mobiles->get(0); // Get the first mobile template
+	}
+	
+	// Get the creature's display name
+	String targetName = "creature";
+	if (!targetTemplate.isEmpty()) {
+		CreatureTemplate* creatureTemplate = CreatureTemplateManager::instance()->getTemplate(targetTemplate);
+		if (creatureTemplate != nullptr) {
+			targetName = creatureTemplate->getObjectName();
+		}
+	}
+	
+	// Set the mission target name to the creature's name
+	mission->setMissionTargetName(targetName);
+	
+	// Set a generic mission title
 	mission->setMissionTitle("mission/mission_destroy_neutral" + messageDifficulty + missionType, "m" + String::valueOf(randTexts) + "t");
 	mission->setMissionDescription("mission/mission_destroy_neutral" +  messageDifficulty + missionType, "m" + String::valueOf(randTexts) + "d");
 
