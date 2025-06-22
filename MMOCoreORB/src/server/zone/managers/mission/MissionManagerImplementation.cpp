3f8d1e971d00b250d5341888a5a0750ffe4b38ca
KimoSWG <crimson1125@gmail.com>
2025-06-21 19:44:53 -0500

    destroy name missions revert.  it didnt work

--- MMOCoreORB/src/server/zone/managers/mission/MissionManagerImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/mission/MissionManagerImplementation.cpp
@@ -913,17 +913,7 @@ void MissionManagerImplementation::randomizeGenericDestroyMission(CreatureObject
 	mission->setStartPosition(startPos.getX(), startPos.getY(), zone->getZoneName());
 	mission->setCreatorName(nm->makeCreatureName());
 
-	// Resolve the string ID to get the proper localized name
-	String lairNameString = "@lair_n:" + lairTemplateObject->getName();
-	UnicodeString resolvedName = StringIdManager::instance()->getStringId(lairNameString.hashCode());
-	
-	// If the string ID couldn't be resolved, fall back to the original string ID
-	if (resolvedName.isEmpty()) {
-		mission->setMissionTargetName(lairNameString);
-	} else {
-		mission->setMissionTargetName(resolvedName.toString());
-	}
-	
+	mission->setMissionTargetName("@lair_n:" + lairTemplateObject->getName());
 	mission->setTargetTemplate(templateObject);
 	mission->setTargetOptionalTemplate(lairTemplate);
 
