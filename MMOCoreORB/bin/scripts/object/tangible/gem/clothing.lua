a4e0e6d032ab7f00f7011880112dc5d5c67ae30f
KimoSWG <crimson1125@gmail.com>
2025-06-24 20:45:01 -0500

    RTZ

--- MMOCoreORB/bin/scripts/object/tangible/gem/clothing.lua
+++ MMOCoreORB/bin/scripts/object/tangible/gem/clothing.lua
@@ -2,14 +2,4 @@ object_tangible_gem_clothing = object_tangible_gem_shared_clothing:new {
 
 }
 
-function object_tangible_gem_clothing:onObjectReady(player)
-    local mods = self:getSkillMods()
-    for modName, _ in pairs(mods) do
-        local label = modName:gsub("_", " "):gsub("(%l)(%w*)", function(a,b) return a:upper()..b end)
-        self:setCustomObjectName(label)
-        self:setCustomName(label)
-        break
-    end
-end
-
 ObjectTemplates:addTemplate(object_tangible_gem_clothing, "object/tangible/gem/clothing.iff")
