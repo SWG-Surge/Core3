059e429fe1d62fe5809e8dd1352f762e087bbe77
KimoSWG <crimson1125@gmail.com>
2025-06-26 06:04:10 -0500

    2

--- MMOCoreORB/src/server/zone/objects/resource/ResourceContainerImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/resource/ResourceContainerImplementation.cpp
@@ -154,7 +154,7 @@ void ResourceContainerImplementation::split(int newStackSize) {
         String resourceClass = spawnObject->getType();
         StringId classNameId("craft_resource_ingredients_n", resourceClass);
         UnicodeString resolvedClassNameUS = StringIdManager::instance()->getStringId(classNameId);
-        String resolvedClassName = resolvedClassNameUS.isEmpty() ? resourceClass : resolvedClassNameUS.toString();
+        String resolvedClassName = resolvedClassNameUS.isEmpty() ? formatFallbackClassName(resourceClass) : resolvedClassNameUS.toString();
         String displayName = resourceName + " (" + resolvedClassName + ")";
 
         newResource->setObjectName(classNameId, false);
