1b8025092048afd19e6837a1c8aa555f87744279
KimoSWG <crimson1125@gmail.com>
2025-06-28 14:29:11 -0500

    chatgpt suuuucks.

--- MMOCoreORB/src/server/zone/objects/player/sui/callbacks/StructurePayMaintenanceSuiCallback.h
+++ MMOCoreORB/src/server/zone/objects/player/sui/callbacks/StructurePayMaintenanceSuiCallback.h
@@ -1,82 +1,31 @@
-/*
- * StructurePayMaintenanceSuiCallback.h
- *
- *  Created on: Aug 16, 2011
- *      Author: cRush
- */
-
-#ifndef STRUCTUREPAYMAINTENANCESUICALLBACK_H_
-#define STRUCTUREPAYMAINTENANCESUICALLBACK_H_
+#ifndef STRUCTUREMAINTENANCEPAYMENTCHOICESUICALLBACK_H_
+#define STRUCTUREMAINTENANCEPAYMENTCHOICESUICALLBACK_H_
 
 #include "server/zone/objects/player/sui/SuiCallback.h"
-#include "server/zone/objects/creature/CreatureObject.h"
-#include "server/zone/objects/structure/StructureObject.h"
 #include "server/zone/managers/structure/StructureManager.h"
 
-
-class StructurePayMaintenanceSuiCallback : public SuiCallback {
+class StructureMaintenancePaymentChoiceSuiCallback : public SuiCallback {
 public:
-	bool useBank;
-	StructurePayMaintenanceSuiCallback(ZoneServer* serv, bool useBank = false) : SuiCallback(serv), useBank(useBank) {
+	StructureMaintenancePaymentChoiceSuiCallback(ZoneServer* serv) : SuiCallback(serv) {}
+
+	void run(CreatureObject* creature, SuiBox* suiBox, uint32 eventIndex, Vector<UnicodeString>* args) override {
+		if (creature == nullptr || suiBox == nullptr || eventIndex == 0)
+			return;
+
+		ManagedReference<SceneObject*> usingObject = suiBox->getUsingObject();
+		if (usingObject == nullptr || !usingObject->isStructureObject())
+			return;
+
+		StructureObject* structure = cast<StructureObject*>(usingObject.get());
+
+		bool useBank = (eventIndex == 2); // 1 = cash, 2 = bank
+
+		StructureManager::instance()->openMaintenancePaymentTransfer(structure, creature, useBank);
 	}
 
-
-	void run(CreatureObject* creature, SuiBox* sui, uint32 eventIndex, Vector<UnicodeString>* args) {
-		bool cancelPressed = (eventIndex == 1);
-
-		if (!sui->isTransferBox() || cancelPressed || args->size() < 2)
-			return;
-
-		int amount = Integer::valueOf(args->get(1).toString());
-
-		if (amount < 0)
-			return;
-
-		ManagedReference<SceneObject*> obj = sui->getUsingObject().get();
-
-		if (obj == nullptr || !obj->isStructureObject()) {
-			creature->sendSystemMessage("@player_structure:invalid_target"); // "Your original structure target is no longer valid. Aborting..."
-			return;
-		}
-
-		//Deposit/Withdraw the maintenance
-		StructureObject* structure = cast<StructureObject*>(obj.get());
-
-		ManagedReference<Zone*> zone = structure->getZone();
-
-		if (zone == nullptr)
-			return;
-
-		//Creature is already locked (done in handleSuiEventNotification in SuiManager).
-		Locker _lock(structure, creature);
-
-		if (amount > 0) {
-	if (useBank) {
-		if (creature->getBankCredits() < amount) {
-			creature->sendSystemMessage("@player_structure:insufficient_funds");
-			return;
-		}
-
-		TransactionLog trx(creature, structure, TrxCode::STRUCTUREMAINTANENCE, amount, true);
-		creature->subtractBankCredits(amount);
-	} else {
-		if (creature->getCashCredits() < amount) {
-			creature->sendSystemMessage("@player_structure:insufficient_funds");
-			return;
-		}
-
-		TransactionLog trx(creature, structure, TrxCode::STRUCTUREMAINTANENCE, amount, true);
-		creature->subtractCashCredits(amount);
-	}
-
-	structure->addMaintenance(amount);
-
-	StringIdChatParameter msg("base_player", "prose_pay_success"); // "You successfully make a payment of %DI credits to %TT."
-	msg.setDI(amount);
-	msg.setTT(structure->getDisplayedName());
-
-	creature->sendSystemMessage(msg);
+	void destroy() override {
+		delete this;
 	}
 };
 
-#endif /* STRUCTUREPAYMAINTENANCESUICALLBACK_H_ */
+#endif /* STRUCTUREMAINTENANCEPAYMENTCHOICESUICALLBACK_H_ */
