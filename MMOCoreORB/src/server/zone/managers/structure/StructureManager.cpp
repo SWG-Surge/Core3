ed61c7b80fa1487abca50a273d77ce32fde197bd
KimoSWG <crimson1125@gmail.com>
2025-06-28 13:41:26 -0500

    cash + bank combined

--- MMOCoreORB/src/server/zone/managers/structure/StructureManager.cpp
+++ MMOCoreORB/src/server/zone/managers/structure/StructureManager.cpp
@@ -1197,7 +1197,7 @@ void StructureManager::promptPayUncondemnMaintenance(CreatureObject* creature, S
 }
 
 void StructureManager::promptPayMaintenance(StructureObject* structure, CreatureObject* creature, SceneObject* terminal) {
-	int availableCredits = creature->getCashCredits();
+	int availableCredits = creature->getCashCredits() + creature->getBankCredits();
 
 	if (availableCredits <= 0) {
 		creature->sendSystemMessage("@player_structure:no_money"); // You do not have any money to pay maintenance.
