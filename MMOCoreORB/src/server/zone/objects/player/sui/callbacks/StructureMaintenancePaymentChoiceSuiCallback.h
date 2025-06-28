c1805ef5979249820a5519e77cab30eaac217aca
KimoSWG <crimson1125@gmail.com>
2025-06-28 16:18:37 -0500

    ok and cancel

--- MMOCoreORB/src/server/zone/objects/player/sui/callbacks/StructureMaintenancePaymentChoiceSuiCallback.h
+++ MMOCoreORB/src/server/zone/objects/player/sui/callbacks/StructureMaintenancePaymentChoiceSuiCallback.h
@@ -9,28 +9,25 @@ public:
 	StructureMaintenancePaymentChoiceSuiCallback(ZoneServer* serv) : SuiCallback(serv) {}
 
 	void run(CreatureObject* creature, SuiBox* suiBox, uint32 eventIndex, Vector<UnicodeString>* args) override {
-		Logger::console.info("DEBUG: StructureMaintenancePaymentChoiceSuiCallback::run called with eventIndex = " + String::valueOf(eventIndex));
+	Logger::console.info("DEBUG: StructureMaintenancePaymentChoiceSuiCallback::run called with eventIndex = " + String::valueOf(eventIndex));
 
-		if (creature == nullptr || suiBox == nullptr || eventIndex != 1)
-			return;
+	if (creature == nullptr || suiBox == nullptr)
+		return;
 
-		ManagedReference<SceneObject*> usingObject = suiBox->getUsingObject();
-		if (usingObject == nullptr || !usingObject->isStructureObject())
-			return;
+	if (args == nullptr || args->size() == 0)
+		return;
 
-		if (args == nullptr || args->size() == 0)
-			return;
+	ManagedReference<SceneObject*> usingObject = suiBox->getUsingObject();
+	if (usingObject == nullptr || !usingObject->isStructureObject())
+		return;
 
-		int selection = Integer::valueOf(args->get(0).toString());
-		bool useBank = (selection == 1); // 0 = cash, 1 = bank
+	int selection = Integer::valueOf(args->get(0).toString());
+	bool useBank = (selection == 1); // 0 = cash, 1 = bank
 
-		StructureObject* structure = cast<StructureObject*>(usingObject.get());
-		StructureManager::instance()->openMaintenancePaymentTransfer(structure, creature, useBank);
-	}
+	StructureObject* structure = cast<StructureObject*>(usingObject.get());
+	StructureManager::instance()->openMaintenancePaymentTransfer(structure, creature, useBank);
+    }
 
-	void destroy() override {
-		delete this;
-	}
 };
 
 #endif /* STRUCTUREMAINTENANCEPAYMENTCHOICESUICALLBACK_H_ */
